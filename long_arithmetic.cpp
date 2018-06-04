7bffb1e08a12963243488eebd6c742691970e97c
Zaharov Andrey <andrey150399@mail.ru>
2018-06-04 15:41:23 +0300

    - remove using non-const operator[] for optimization - rewrite unsigned_div(digit) without temporary objects

--- long_arithmetic.cpp
+++ long_arithmetic.cpp
@@ -10,23 +10,23 @@
 #include "big_integer.h"
 
 
-void big_integer::subtract(big_integer const &rhs){
+void big_integer::subtract(big_integer const &rhs) {
     size_t max_size = std::max(digits.size(), rhs.digits.size()) + 1;
     size_t rhs_size = rhs.digits.size();
     digits.resize(max_size, digits.leading());
     digit carry = 1;
-    digit *data_ptr = digits.begin();
+    digit *this_ptr = digits.begin();
 
     for (size_t i = 0; i < rhs_size; ++i) {
         digit cur_rhs = ~rhs.digits[i];
-        carry = add_with_overflow(data_ptr[i], carry);
-        carry += add_with_overflow(data_ptr[i], cur_rhs);
+        carry = add_with_overflow(this_ptr[i], carry);
+        carry += add_with_overflow(this_ptr[i], cur_rhs);
     }
     digit lead = ~rhs.digits.leading();
 
-    for (size_t i = rhs_size; i < max_size; i++){
-        carry = add_with_overflow(data_ptr[i], carry);
-        carry += add_with_overflow(data_ptr[i], lead);
+    for (size_t i = rhs_size; i < max_size; i++) {
+        carry = add_with_overflow(this_ptr[i], carry);
+        carry += add_with_overflow(this_ptr[i], lead);
     }
 
     digits.set_sign(leading_bit(digits.back()));
@@ -49,7 +49,7 @@ void big_integer::add(big_integer const &rhs) {
     }
     digit lead = rhs.digits.leading();
 
-    for (size_t i = rhs_size; i < max_size; i++){
+    for (size_t i = rhs_size; i < max_size; i++) {
         carry = add_with_overflow(data_ptr[i], carry);
         carry += add_with_overflow(data_ptr[i], lead);
     }
@@ -87,8 +87,9 @@ int cmp(big_integer const &a, big_integer const &b) { // (a - b) = res * |a - b|
 
 
 void big_integer::bit_negate() {
+    digit *this_ptr = digits.begin();
     for (size_t i = 0; i < digits.size(); i++) {
-        digits[i] = ~digits[i];
+        this_ptr[i] = ~this_ptr[i];
     }
     digits.set_sign(!digits.is_negative());
 }
@@ -128,45 +129,38 @@ void big_integer::unsigned_mul(big_integer const &rhs) {
     const big_integer &b = rhs;
     *this = 0;
     digits.resize(a.digits.size() + b.digits.size() + 1);
+    digit *this_ptr = digits.begin();
     for (size_t i = 0; i < a.digits.size(); i++) {
         digit cur_a = a.digits[i];
         digit carry = 0;
         for (size_t j = 0; j < b.digits.size(); j++) {
             digit cur_b = b.digits[j];
-            carry = add_with_overflow(digits[i + j], carry);
+            carry = add_with_overflow(this_ptr[i + j], carry);
             carry += mul_with_overflow(cur_b, cur_a);
-            carry += add_with_overflow(digits[i + j], cur_b);
+            carry += add_with_overflow(this_ptr[i + j], cur_b);
         }
-        digits[i + b.digits.size()] = carry;
+        this_ptr[i + b.digits.size()] = carry;
     }
     pop_zeros();
 }
 
 
 digit estimate(digit r0, digit r1, const digit d) {
-    static const digit HALF = BITS / 2;
     __uint128_t r2 = r1 + (static_cast<__uint128_t>(r0) << BITS);
     return (digit) std::min(r2 / d, static_cast<__uint128_t>(~0ull));
 }
 
 
 big_integer big_integer::unsigned_div_mod(digit rhs) {
-    big_integer const a = *this;
-    big_integer const b = rhs;
-    digits.resize(0);
-    big_integer rem = 0;
-    for (int64_t i = a.digits.size() - 1; i >= 0; i--) {
-        rem <<= BITS;
-        rem.digits[0] = a.digits[i];
-        digit est = estimate(rem.digits.unbound_get(1), rem.digits.unbound_get(0), rhs);
-        big_integer tmp = b;
-        tmp.unsigned_mul(est);
-        rem -= tmp;
-        digits.push_back(est);
+    digit carry = 0;
+    digit *this_ptr = digits.begin();
+    for (int64_t i = digits.size() - 1; i >= 0; i--) {
+        __uint128_t cur = this_ptr[i] + (static_cast<__uint128_t>(carry) << BITS);
+        this_ptr[i] = digit(cur / rhs);
+        carry = digit(cur % rhs);
     }
-    std::reverse(digits.begin(), digits.end());
     pop_zeros();
-    return rem;
+    return carry;
 }
 
 
@@ -192,21 +186,21 @@ big_integer big_integer::unsigned_div_mod(big_integer const &rhs) {
 
     auto n = static_cast<int64_t>(a.digits.size());
     auto m = static_cast<int64_t>(b.digits.size());
-    digits.resize(n - m + 1);
+    digits.resize(static_cast<size_t>(n - m + 1));
     digit *ptr_a = a.digits.begin();
-    digit *ptr_this = digits.begin();
+    digit *this_ptr = digits.begin();
     digit b_lead = b.digits[m - 1];
     big_integer rem = 0;
 
-    rem.digits.resize(m + 1);
+    rem.digits.resize(static_cast<size_t>(m + 1));
     std::copy(ptr_a + n - m + 1, ptr_a + n, rem.digits.begin());
 
     for (int64_t i = n - m; i >= 0; i--) {
         rem <<= BITS;
         rem.digits[0] = ptr_a[i];
         digit est = estimate(
-                rem.digits.unbound_get(m),
-                rem.digits.unbound_get(m - 1),
+                rem.digits.unbound_get(static_cast<size_t>(m)),
+                rem.digits.unbound_get(static_cast<size_t>(m - 1)),
                 b_lead);
         big_integer tmp = b;
         tmp.unsigned_mul(est);
@@ -216,8 +210,8 @@ big_integer big_integer::unsigned_div_mod(big_integer const &rhs) {
         }
         rem -= tmp;
         assert(rem < b);
-        *ptr_this = est;
-        ptr_this++;
+        *this_ptr = est;
+        this_ptr++;
     }
     std::reverse(digits.begin(), digits.end());
     rem.unsigned_div_mod(scale);
@@ -232,20 +226,21 @@ void big_integer::shift_left(int shift) {
     int64_t digit_shift = shift / BITS;
     digits.resize(digits.size() + digit_shift + 1);
     size_t n = digits.size();
+    digit *this_ptr = digits.begin();
     if (digit_shift > 0) {
         for (int64_t i = n - 1; i >= digit_shift; i--) {
-            digits[i] = digits[i - digit_shift];
+            this_ptr[i] = this_ptr[i - digit_shift];
         }
         for (int64_t i = digit_shift - 1; i >= 0; i--) {
-            digits[i] = 0;
+            this_ptr[i] = 0;
         }
     }
     if (bit_shift > 0) {
         for (int64_t i = n - 1; i > 0; i--) {
-            digits[i] <<= bit_shift;
-            digits[i] |= digits[i - 1] >> (BITS - bit_shift);
+            this_ptr[i] <<= bit_shift;
+            this_ptr[i] |= this_ptr[i - 1] >> (BITS - bit_shift);
         }
-        digits[0] <<= bit_shift;
+        this_ptr[0] <<= bit_shift;
     }
     pop_zeros();
 }
@@ -254,13 +249,19 @@ void big_integer::shift_left(int shift) {
 void big_integer::shift_right(int shift) {
     size_t bit_shift = shift % BITS;
     size_t digit_shift = shift / BITS;
-    for (size_t i = 0; i < digits.size() - digit_shift; i++) {
-        digits[i] = digits[i + digit_shift];
+    if (digit_shift > 0) {
+        digit *this_ptr = digits.begin();
+        for (size_t i = 0; i < digits.size() - digit_shift; i++) {
+            this_ptr[i] = this_ptr[i + digit_shift];
+        }
     }
     digits.resize(digits.size() - digit_shift + 1);
-    for (size_t i = 0; i < digits.size() - 1; i++) {
-        digits[i] >>= bit_shift;
-        digits[i] |= digits[i + 1] << (BITS - bit_shift);
+    if (bit_shift > 0) {
+        digit *this_ptr = digits.begin();
+        for (size_t i = 0; i < digits.size() - 1; i++) {
+            this_ptr[i] >>= bit_shift;
+            this_ptr[i] |= this_ptr[i + 1] << (BITS - bit_shift);
+        }
     }
     digits.pop_back();
     pop_zeros();
